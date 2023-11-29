7d735ba7742202ebe77306f41491176ddb7a8ceb
SANGWOO PARK <cyberprophet@hanmail.net>
2023-11-30 00:47:31 +0900

    with location information?

---
+++ OpenAPI.Console/Geolocation.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+using RestSharp;
+
+namespace ShareInvest;
+
+class Geolocation : RestClient
+{
+    internal async Task ExecutePostAsync(string resource)
+    {
+        var request = new RestRequest(resource, Method.Post);
+
+        var json = JsonConvert.SerializeObject(new
+        {
+            considerIp = false,
+            wifiAccessPoints = new[]
+            {
+                new
+                {
+                    macAddress = "3c:37:86:5d:75:d4",
+                    signalStrength = -35,
+                    signalToNoiseRatio = 0
+                },
+                new
+                {
+                    macAddress = "94:b4:0f:fd:c1:40",
+                    signalStrength = -35,
+                    signalToNoiseRatio = 0
+                }
+            }
+        });
+        var res = await ExecuteAsync(request.AddJsonBody(json), cts.Token);
+
+        Console.WriteLine(res);
+    }
+    internal Geolocation(string baseUrl) : base(baseUrl)
+    {
+
+    }
+    readonly CancellationTokenSource cts = new();
+}
