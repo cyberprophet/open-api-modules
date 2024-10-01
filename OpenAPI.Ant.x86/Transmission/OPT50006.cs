39f0ac1c1aaa856d0567661c5193415c2363d98b
sangwoo park <cyberprophet@hanmail.net>
2024-10-01 17:53:26 +0900

    change in real-time registration method for futures option

---
+++ OpenAPI.Ant.x86/Transmission/OPT50006.cs
@@ -0,0 +1,36 @@
+﻿using AxKHOpenAPILib;
+
+using Newtonsoft.Json;
+
+namespace ShareInvest.Transmission;
+
+/// <summary>선옵체결추이요청</summary>
+class OPT50006 : Constructor
+{
+    internal override IEnumerable<string> OnReceiveTrData(AxKHOpenAPI axAPI, _DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+    {
+        if (Id == null || Value == null)
+        {
+            yield break;
+        }
+        var singleData = OnReceiveTrSingleData(axAPI, e);
+
+        foreach (var storage in OnReceiveTrMultiData(axAPI, e))
+        {
+            if (storage.Count == 0)
+            {
+                continue;
+            }
+
+            foreach (var single in singleData)
+            {
+                storage[single.Key] = single.Value;
+            }
+            storage[Id[0]] = Value[0];
+            storage[nameof(Entities.Kiwoom.OPT50006.Date)] = DateTime.Now.ToString("yyyyMMdd", TrConstructor.Culture);
+
+            yield return JsonConvert.SerializeObject(storage);
+        }
+        yield return e.sPrevNext;
+    }
+}
